ca19f07f042670d5ea977db60d69c24fc953446c
zhangzheng-zz <15521414675@163.com>
2020-10-16 19:31:47 +0800

    feat(App): ref reactive

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-### 开始
-初始化项目
+## 开始
+### 初始化项目
+
+使用 `vuecli` 生成 `vue3` + `ts` 环境
+
+### 版本控制
+
+教程：https://github.com/buddywang/blog/issues/19
+
+```js
+"pre_build": "standard-version --prerelease alpha",
+"patch_build": "standard-version --release-as patch",
+"minor_build": "standard-version --release-as minor",
+"major_build": "standard-version --release-as major"
+```
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <h2>欢迎XXXX</h2>
+    <div>请选择一位XXX</div>
+  </div>
+  <div>
+    <button
+      v-for="(item, index) in singer"
+      :key="index"
+      @click="selectSingerFun(index)"
+    >
+      {{ index }} : {{ item }}
+    </button>
+    <div>你选择了【{{ selectSinger }}】</div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+interface SingerData {
+  singer: string[];
+  selectSinger: string;
+  selectSingerFun: (index: number) => void;
+}
+
+import {
+  defineComponent,
+  ref,
+  reactive,
+  toRefs,
+  onRenderTracked,
+  onRenderTriggered,
+} from "vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+
+  // 使用 ref
+  // setup() {
+  //   const singer = ref(["邓紫棋", "薛之谦", "李荣浩"]);
+  //   const selectSinger = ref("");
+  //   const selectSingerFun = (index: number) => {
+  //     selectSinger.value = singer.value[index];
+  //   };
+  //   return {
+  //     singer,
+  //     selectSinger,
+  //     selectSingerFun
+  //   };
+  // },
+
+  setup() {
+    // onRenderTracked((event) => {
+    //   console.log("状态跟踪组件----------->");
+    //   console.log(event);
+    // });
+
+    onRenderTriggered((event) => {
+      console.log("状态触发组件--------------->");
+      console.log(event);
+    });
+
+    const singerData: SingerData = reactive({
+      singer: ["邓紫棋", "薛之谦", "李荣浩"],
+      selectSinger: "",
+      selectSingerFun: (index: number) => {
+        singerData.selectSinger = singerData.singer[index];
+      },
+    });
+    const data = toRefs(singerData);
+    return {
+      ...data,
+    };
+  },
 });
 </script>
 
